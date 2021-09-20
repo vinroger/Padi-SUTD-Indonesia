4b19df291e701fbdb44e64d4971db0839c0ec58b
root <root@3io6.l.time4vps.cloud>
2021-09-20 07:54:16 +0300

    remove scrollbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
-import {MenuIcon } from '@heroicons/react/solid';
+import { MenuIcon } from '@heroicons/react/solid';
 import React, { useState } from 'react';
 
 
@@ -9,86 +9,89 @@ import bg1 from '../public/img/event001.jpg'
 
 
 
-export default function Layout({children}) {
-    let [clicked,setClicked] = useState(false);
-    return <div>
-    <Head>
-        <title>PADI SUTD</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>                                                
-        <nav style={{"backgroundColor":"#5E1212"}}>
-            <div className="max-w-full pt-4 pb-4 mx-auto px-4 block md:flex"> 
-                
-                <div className = "flex md:ml-10 pl-5 justify-between pr-5 items-center"> 
-                    {/* logo padi */}   
-                    <Link href="/">
-                    <a className = "hover:opacity-50 flex items-center"> 
-                        <img src="./img/logo-large.png" width="100rem"/>
-                    </a>
+export default function Layout({ children }) {
+    let [clicked, setClicked] = useState(false);
+    return <div className="overflow-hidden">
+        <Head>
+            <title>PADI SUTD</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="description" content="Perhimpunan Mahasiswa Indonesia di Singapore University of Technology and Design (PADI SUTD). Learn more about us." />
+            <meta name="keywords" content="PADI SUTD Perhimpunan Mahasiswa Indonesia di Singapore" />
+        </Head>
+        <header>
+            <h1 className="hidden">PADI SUTD Perhimpunan Mahasiswa Indonesia di Singapore</h1>
+            <nav style={{ "backgroundColor": "#5E1212" }}>
+                <div className="max-w-full pt-4 pb-4 mx-auto px-4 block md:flex">
 
-                    </Link>
-                    {/* logo burger */}
-                    <div className="md:hidden" >
-                    <MenuIcon className="h-8 text-white" onClick={()=> setClicked(!clicked)}></MenuIcon>
+                    <div className="flex md:ml-10 pl-5 justify-between pr-5 items-center">
+                        {/* logo padi */}
+                        <Link href="/">
+                            <a className="hover:opacity-50 flex items-center">
+                                <img src="./img/logo-large.png" width="100rem" />
+                            </a>
+
+                        </Link>
+                        {/* logo burger */}
+                        <div className="md:hidden" >
+                            <MenuIcon className="h-8 text-white" onClick={() => setClicked(!clicked)}></MenuIcon>
+
+                        </div>
+                    </div>
+                    <div className="hidden md:flex ml-auto mr-10 pr-5 items-center space-x-10 text-white">
+                        <Link href="/" ><a className="font-semi-bold text-xl hover:opacity-50 hover:animate-bounce">Home</a></Link>
+                        <Link href="/events" ><a className="font-semi-bold text-xl hover:opacity-50 hover:animate-bounce">Events</a></Link>
+                        <Link href="/committee" ><a className="font-semi-bold text-xl hover:opacity-50 hover:animate-bounce">Committee</a></Link>
+                        <Link href="/" ><a className="font-semi-bold text-xl hover:opacity-50 hover:animate-bounce">Contact Us</a></Link>
 
                     </div>
+
+                    <div className={clicked ? "" : "hidden"}>
+                        <div className="md:hidden text-white pl-5 ml-5 space-y-5 mt-8">
+                            <Link href="/" ><a className="font-semi-bold text-xl block hover:opacity-50">Home</a></Link>
+                            <Link href="/events" ><a className="font-semi-bold text-xl block hover:opacity-50">Events</a></Link>
+                            <Link href="/committee" ><a className="font-semi-bold text-xl block hover:opacity-50">Committee</a></Link>
+                            <Link href="/" ><a className="font-semi-bold text-xl block hover:opacity-50">Contact Us</a></Link>
+                        </div>
+                    </div>
+
+
+
+
                 </div>
-                <div className="hidden md:flex ml-auto mr-10 pr-5 items-center space-x-10 text-white"> 
-                    <Link href="/" ><a className="font-semi-bold text-xl hover:opacity-50 hover:animate-bounce">Home</a></Link>
-                    <Link href="/events" ><a className="font-semi-bold text-xl hover:opacity-50 hover:animate-bounce">Events</a></Link>
-                    <Link href="/committee" ><a className="font-semi-bold text-xl hover:opacity-50 hover:animate-bounce">Committee</a></Link>
-                    <Link href="/" ><a className="font-semi-bold text-xl hover:opacity-50 hover:animate-bounce">Contact Us</a></Link>
-                
-                </div>
-                
-                <div className={clicked? "" : "hidden"}>
-                    <div className="md:hidden text-white pl-5 ml-5 space-y-5 mt-8"> 
-                        <Link href="/" ><a className="font-semi-bold text-xl block hover:opacity-50">Home</a></Link>
-                        <Link href="/events" ><a className="font-semi-bold text-xl block hover:opacity-50">Events</a></Link>
-                        <Link href="/committee" ><a className="font-semi-bold text-xl block hover:opacity-50">Committee</a></Link>
-                        <Link href="/" ><a className="font-semi-bold text-xl block hover:opacity-50">Contact Us</a></Link>
+
+
+            </nav>
+        </header>
+
+
+
+        <div>{children}</div>
+        <footer>
+            <div className="w-full h-auto p-10" style={{ "backgroundColor": "#5E1212" }}>
+                <div className="flex flex-col items-center md:flex-row md:space-y-5 justify-around pl-10 pr-10">
+                    <div className="relative w-48 h-36 flex hover:opacity-50 hover:cursor-pointer">
+                        <Link href="/">
+                            <Image src="/img/logo-large.png" layout="fill" objectFit="contain"></Image>
+                        </Link>
+
+
+                    </div>
+                    <div className="text-white justify-evenly space-x-10 flex flex-grow max-w-lg">
+                        <Link href="/" ><a className="font-semi-bold text-xl hover:opacity-50">Home</a></Link>
+                        <Link href="/events" ><a className="font-semi-bold text-xl hover:opacity-50">Events</a></Link>
+                        <Link href="/committee" ><a className="font-semi-bold text-xl hover:opacity-50">Committee</a></Link>
+                        <Link href="/" ><a className="font-semi-bold text-xl hover:opacity-50">Contact Us</a></Link>
                     </div>
                 </div>
-                
-
-            
-            
-            </div>
-        
-        
-        </nav>
-    </header>
-        
-
-    
-    <div>{children}</div>
-    <footer>
-        <div className="w-full h-auto p-10" style={{"backgroundColor":"#5E1212"}}>
-            <div className="flex flex-col items-center md:flex-row md:space-y-5 justify-around pl-10 pr-10">
-                <div className="relative w-48 h-36 flex hover:opacity-50 hover:cursor-pointer">
-                    <Link href="/">
-                        <Image src="/img/logo-large.png" layout="fill" objectFit="contain"></Image>
-                    </Link>
-
-                    
+                <div className="text-center mt-5 pt-10">
+                    <div className="text-white text-xs opacity-50">
+                        ©2021 Perhimpunan Mahasiswa SUTD Indonesia. All right reserved.
+                    </div>
                 </div>
-                <div className="text-white justify-evenly space-x-10 flex flex-grow max-w-lg">
-                    <Link href="/" ><a className="font-semi-bold text-xl hover:opacity-50">Home</a></Link>
-                    <Link href="/events" ><a className="font-semi-bold text-xl hover:opacity-50">Events</a></Link>
-                    <Link href="/committee" ><a className="font-semi-bold text-xl hover:opacity-50">Committee</a></Link>
-                    <Link href="/" ><a className="font-semi-bold text-xl hover:opacity-50">Contact Us</a></Link>
-                </div>
-            </div>
-            <div className="text-center mt-5 pt-10">
-                <div className="text-white text-xs opacity-50">
-                    ©2021 Perhimpunan Mahasiswa SUTD Indonesia. All right reserved.
-                </div>
-            </div>
 
-        </div>
-    </footer>
+            </div>
+        </footer>
 
     </div>
-    
+
 }
--- components/Titleimg.js
+++ components/Titleimg.js
@@ -1,24 +1,24 @@
-function Titleimg({children, imgsrc}) {
+function Titleimg({ children, imgsrc }) {
 
-    const styling= {
-    "background-image" : "url('" + imgsrc + "')",
-    "transition": "all 1s ease",
-    "-moz-transition": "all 1s ease",
-    "-ms-transition": "all 1s ease",
-    "-webkit-transition": "all 1s ease",
-    "-o-transition": "all 1s ease",
+    const styling = {
+        "background-image": "url('" + imgsrc + "')",
+        "transition": "all 1s ease",
+        "-moz-transition": "all 1s ease",
+        "-ms-transition": "all 1s ease",
+        "-webkit-transition": "all 1s ease",
+        "-o-transition": "all 1s ease",
     };
     return (
         <div>
-            
+
             <div id="bg-hero" className="bg-no-repeat bg-center bg-cover w-screen h-full" style={styling}>
-            <div className= "bg-black bg-opacity-70 text-white" style={{"width" : "100vw", "height" : "45vw"}} >
-                <div className="flex flex-col text-center align-center justify-center w-full h-full">
-                    {children}
+                <div className="bg-black bg-opacity-70 text-white" style={{ "width": "100vw", "height": "47vw" }} >
+                    <div className="flex flex-col text-center align-center justify-center w-full h-full">
+                        {children}
+                    </div>
                 </div>
-                </div>
-            
-                
+
+
             </div>
         </div>
     )
